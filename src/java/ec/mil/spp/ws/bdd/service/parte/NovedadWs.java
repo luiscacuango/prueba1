aaa5a8bce95ba2fd25325b203fe8a28009a02cea
user <user@laptop>
2017-10-10 10:24:24 -0500

    Se cambia los metodos a las clases correspondientes, para una mejor organizacion y poder mandar a llamar los metodos de personal disponible y con novedades cuando se requiera

--- src/java/ec/mil/spp/ws/bdd/service/parte/NovedadWs.java
+++ src/java/ec/mil/spp/ws/bdd/service/parte/NovedadWs.java
@@ -4,6 +4,7 @@ import ec.mil.spp.ws.bdd.PlypLiceperm;
 import ec.mil.spp.ws.bdd.PperPersona;
 import ec.mil.spp.ws.bdd.PtlpTiplicper;
 import ec.mil.spp.ws.bdd.dto.NovedadDTO;
+import ec.mil.spp.ws.bdd.dto.PersonaDTO;
 import ec.mil.spp.ws.bdd.dto.TipoNovedadDTO;
 import java.math.BigInteger;
 import java.util.ArrayList;
@@ -11,6 +12,7 @@ import java.util.Date;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -18,6 +20,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -86,4 +89,123 @@ public class NovedadWs {
         return "Novedad registrada exitosamente";
     }
 
+    /**
+     *  Metodo para consultar al personal Disponible
+     * @param fechaBus
+     * @param perSecuen
+     * @return
+     * @throws Exception
+     */
+    @GET
+    @Path("consultarPerDisponible")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<PersonaDTO> consultarPersonaDisponible(@QueryParam(value = "fechaBus") Date fechaBus, 
+            @QueryParam(value = "perSecuen") Long perSecuen) throws Exception{
+            /***********************************************************************************
+             * Consultar Personal Disponible
+             * ********************************************************************************/
+        PperPersona persona = em.find(PperPersona.class, perSecuen);
+            List<PersonaDTO> listPersonal = new ArrayList<>();
+            for (PperPersona perTmp:buscarPersonalPorGrupo(persona.getFunSecuen().getUniSecuen().getGdoSecuen().getGdoSecuen(), 
+                     fechaBus)) {
+                PersonaDTO personaDTO = new PersonaDTO();
+                personaDTO.setPerSecuen(perTmp.getPerSecuen());
+                personaDTO.setPerCedula(perTmp.getPerCedula());
+                personaDTO.setPerApellido(perTmp.getPerApellido());
+                personaDTO.setPerNombre(perTmp.getPerNombre());
+                personaDTO.setNombreGrado(perTmp.getGmiSecuen().getGmiAbreviat());
+                personaDTO.setTipoEfectivo(perTmp.getGmiSecuen().getGmiTipoefec().toString());
+                listPersonal.add(personaDTO);
+            }      
+            return listPersonal;
+    }
+    
+    /**
+     *  Metodo para consultar al Personal con Novedad
+     * @param fechaBus
+     * @param perSecuen
+     * @return
+     * @throws Exception
+     */
+    @GET
+    @Path("consultarPerNovedad")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<PersonaDTO> consultarPersonalNovedad(@QueryParam(value = "fechaBus") Date fechaBus, 
+            @QueryParam(value = "perSecuen") Long perSecuen) throws Exception{
+            /***********************************************************************************
+             * Cargar Personal con Novedades
+             * ********************************************************************************/
+        PperPersona persona = em.find(PperPersona.class, perSecuen);
+            List<PersonaDTO> listPersonalNov = new ArrayList<>();
+            for (PlypLiceperm licPerTmp:cargaNovedades(persona.getFunSecuen().getUniSecuen().getGdoSecuen().getGdoSecuen(), 
+                     fechaBus)) {
+                PersonaDTO personaDTO = new PersonaDTO();
+                personaDTO.setPerSecuen(licPerTmp.getPerSecuen().getPerSecuen());
+                personaDTO.setPerCedula(licPerTmp.getPerSecuen().getPerCedula());
+                personaDTO.setPerApellido(licPerTmp.getPerSecuen().getPerApellido());
+                personaDTO.setPerNombre(licPerTmp.getPerSecuen().getPerNombre());
+                personaDTO.setPerNovedad(licPerTmp.getTlpSecuen().getTlpDescri());
+                personaDTO.setPerFechaInicio(licPerTmp.getLypInicio());
+                personaDTO.setPerFechaFin(licPerTmp.getLypFin());
+                listPersonalNov.add(personaDTO);
+            }            
+            return listPersonalNov;
+    }
+    
+    /**
+     * Método para listar personal DISPONIBLE de un grupo por fecha
+     * @param idGru
+     * @param fecha
+     * @return 
+     */
+    public List<PperPersona> buscarPersonalPorGrupo(Integer idGru, Date fecha) throws Exception {
+        Query conPerGru = em.createNativeQuery("select p.per_secuen, p.per_nombre,"
+                + "      p.per_apellido, gr.gmi_secuen, gr.gmi_grado"
+                + " from personal.pper_persona p, personal.pfun_funcion f,"
+                + "      personal.puni_unidad u, personal.pgdo_grupoope g, "
+                + "      personal.pgmi_gradomil gr"
+                + " where p.per_fecsalida is null and "
+                + "       p.gmi_secuen = gr.gmi_secuen and "
+                + "       p.fun_secuen = f.fun_secuen and "
+                + "       f.uni_secuen = u.uni_secuen and "
+                + "       u.gdo_secuen = g.gdo_secuen and "
+                + "       g.gdo_secuen = ?1 and"
+                + "       p.per_Secuen not in (Select per_secuen from personal.Plyp_Liceperm lyp"
+                + "             where ?2 between lyp.lyp_Inicio::date and lyp.lyp_Fin::date)"
+                + "       order by gr.gmi_orden, p.per_apellido", PperPersona.class);
+        conPerGru.setParameter(1, idGru);
+        conPerGru.setParameter(2, fecha);
+        return conPerGru.getResultList();
+    }
+
+    /**
+     * Método para cargar el parte por día y grupo
+     *
+     * @param idGru
+     * @param fecha
+     * @return
+     */
+    
+    public List<PlypLiceperm> cargaNovedades(Integer idGru, Date fecha) {
+        Query connoved = em.createNativeQuery("Select * "
+                + "from personal.plyp_liceperm lyp, personal.pper_persona p,"
+                + "     personal.pfun_funcion f, personal.puni_unidad u,"
+                + "     personal.pgdo_grupoope g, personal.pgmi_gradomil gr"
+                + "                 where lyp.per_secuen = p.per_Secuen"
+                + "                   and p.gmi_secuen = gr.gmi_secuen"
+                + "                   and p.fun_secuen = f.fun_secuen"
+                + "                   and f.uni_secuen = u.uni_secuen"
+                + "                   and u.gdo_secuen = g.gdo_secuen"
+                + "                   and g.gdo_secuen = ?1"
+                + "                   and ?2 between lyp.lyp_inicio::date and lyp.lyp_fin::date"
+                + "                 order by lyp.lyp_Inicio desc", PlypLiceperm.class);
+//        Query connoved = em.createQuery("Select lyp from PlypLiceperm lyp "
+//                + " where lyp.perSecuen.funSecuen.unidad.grupo.gdoSecuen =:grupo"
+//                + "  and :fecha between lyp.lypInicio and lyp.lypFin"
+//                + " order by lyp.lypInicio desc");
+        connoved.setParameter(1, idGru);
+        connoved.setParameter(2, fecha);
+        return connoved.getResultList();
+    }
+    
 }
--- src/java/ec/mil/spp/ws/bdd/service/parte/SeguridadWs.java
+++ src/java/ec/mil/spp/ws/bdd/service/parte/SeguridadWs.java
@@ -26,10 +26,10 @@ import javax.ws.rs.core.MediaType;
  */
 @Path("seguridad")
 public class SeguridadWs {
-    
+
     @PersistenceContext(unitName = "parteMovilWebPU")
     private EntityManager em;
-    
+
     @GET
     @Path("validarUsuario/{usuario}/{clave}")
     @Produces(MediaType.APPLICATION_JSON)
@@ -58,45 +58,12 @@ public class SeguridadWs {
             usuarioDto.setGrupo(usuarioPer.getPerSecuen().getFunSecuen().getUniSecuen().getGdoSecuen().getGdoAbreviat());
             usuarioDto.setUnidad(usuarioPer.getPerSecuen().getFunSecuen().getUniSecuen().getUniAbreviat());
             usuarioDto.setNombreGrado(usuarioPer.getPerSecuen().getGmiSecuen().getGmiAbreviat());
-            /***********************************************************************************
-             * Cargar Personal Disponible
-             * ********************************************************************************/
-            List<PersonaDTO> listPersonal = new ArrayList<>();
-            for (PperPersona perTmp:buscarPersonalPorGrupo(usuarioPer.getPerSecuen().getFunSecuen().getUniSecuen().getGdoSecuen().getGdoSecuen(), 
-                     new Date())) {
-                PersonaDTO personaDTO = new PersonaDTO();
-                personaDTO.setPerSecuen(perTmp.getPerSecuen());
-                personaDTO.setPerCedula(perTmp.getPerCedula());
-                personaDTO.setPerApellido(perTmp.getPerApellido());
-                personaDTO.setPerNombre(perTmp.getPerNombre());
-                personaDTO.setNombreGrado(perTmp.getGmiSecuen().getGmiAbreviat());
-                personaDTO.setTipoEfectivo(perTmp.getGmiSecuen().getGmiTipoefec().toString());
-                listPersonal.add(personaDTO);
-            }            
-            usuarioDto.setListaPersonal(listPersonal);
-            /***********************************************************************************
-             * Cargar Personal con Novedades
-             * ********************************************************************************/
-            List<PersonaDTO> listPersonalNov = new ArrayList<>();
-            for (PlypLiceperm licPerTmp:cargaNovedades(usuarioPer.getPerSecuen().getFunSecuen().getUniSecuen().getGdoSecuen().getGdoSecuen(), 
-                     new Date())) {
-                PersonaDTO personaDTO = new PersonaDTO();
-                personaDTO.setPerSecuen(licPerTmp.getPerSecuen().getPerSecuen());
-                personaDTO.setPerCedula(licPerTmp.getPerSecuen().getPerCedula());
-                personaDTO.setPerApellido(licPerTmp.getPerSecuen().getPerApellido());
-                personaDTO.setPerNombre(licPerTmp.getPerSecuen().getPerNombre());
-                personaDTO.setPerNovedad(licPerTmp.getTlpSecuen().getTlpDescri());
-                personaDTO.setPerFechaInicio(licPerTmp.getLypInicio());
-                personaDTO.setPerFechaFin(licPerTmp.getLypFin());
-                listPersonalNov.add(personaDTO);
-            }            
-            usuarioDto.setListaPersonalNov(listPersonalNov);
         } catch (Exception e) {
             throw new Exception("Usuario no encontrado");
         }
         return usuarioDto;
     }
-    
+
     @GET
     @Path("validarUsuarioDos")
     @Produces(MediaType.APPLICATION_JSON)
@@ -115,7 +82,7 @@ public class SeguridadWs {
         }
         return usuarioDev;
     }
-    
+
     @GET
     @Path("consultarTodos")
     @Produces(MediaType.APPLICATION_JSON)
@@ -133,65 +100,9 @@ public class SeguridadWs {
             usuarioDto.setPerNombre(object.getPerSecuen().getPerNombre());
             usuarioDto.setPerApellido(object.getPerSecuen().getPerApellido());
             listaUsuDto.add(usuarioDto);
-            
+
         }
         return listaUsuDto;
     }
-    
-    /**
-     * Método para listar personal DISPONIBLE de un grupo por fecha
-     * @param idGru
-     * @param fecha
-     * @return 
-     */
-    public List<PperPersona> buscarPersonalPorGrupo(Integer idGru, Date fecha) throws Exception {
-        Query conPerGru = em.createNativeQuery("select p.per_secuen, p.per_nombre,"
-                + "      p.per_apellido, gr.gmi_secuen, gr.gmi_grado"
-                + " from personal.pper_persona p, personal.pfun_funcion f,"
-                + "      personal.puni_unidad u, personal.pgdo_grupoope g, "
-                + "      personal.pgmi_gradomil gr"
-                + " where p.per_fecsalida is null and "
-                + "       p.gmi_secuen = gr.gmi_secuen and "
-                + "       p.fun_secuen = f.fun_secuen and "
-                + "       f.uni_secuen = u.uni_secuen and "
-                + "       u.gdo_secuen = g.gdo_secuen and "
-                + "       g.gdo_secuen = ?1 and"
-                + "       p.per_Secuen not in (Select per_secuen from personal.Plyp_Liceperm lyp"
-                + "             where ?2 between lyp.lyp_Inicio::date and lyp.lyp_Fin::date)"
-                + "       order by gr.gmi_orden, p.per_apellido", PperPersona.class);
-        conPerGru.setParameter(1, idGru);
-        conPerGru.setParameter(2, fecha);
-        return conPerGru.getResultList();
-    }
 
-    /**
-     * Método para cargar el parte por día y grupo
-     *
-     * @param idGru
-     * @param fecha
-     * @return
-     */
-    
-    public List<PlypLiceperm> cargaNovedades(Integer idGru, Date fecha) {
-        Query connoved = em.createNativeQuery("Select * "
-                + "from personal.plyp_liceperm lyp, personal.pper_persona p,"
-                + "     personal.pfun_funcion f, personal.puni_unidad u,"
-                + "     personal.pgdo_grupoope g, personal.pgmi_gradomil gr"
-                + "                 where lyp.per_secuen = p.per_Secuen"
-                + "                   and p.gmi_secuen = gr.gmi_secuen"
-                + "                   and p.fun_secuen = f.fun_secuen"
-                + "                   and f.uni_secuen = u.uni_secuen"
-                + "                   and u.gdo_secuen = g.gdo_secuen"
-                + "                   and g.gdo_secuen = ?1"
-                + "                   and ?2 between lyp.lyp_inicio::date and lyp.lyp_fin::date"
-                + "                 order by lyp.lyp_Inicio desc", PlypLiceperm.class);
-//        Query connoved = em.createQuery("Select lyp from PlypLiceperm lyp "
-//                + " where lyp.perSecuen.funSecuen.unidad.grupo.gdoSecuen =:grupo"
-//                + "  and :fecha between lyp.lypInicio and lyp.lypFin"
-//                + " order by lyp.lypInicio desc");
-        connoved.setParameter(1, idGru);
-        connoved.setParameter(2, fecha);
-        return connoved.getResultList();
-    }
-    
 }
